b4a54c875ac6fca0b514cd3ee2159d523173353d
s22442 <73021231+s22442@users.noreply.github.com>
2020-11-07 12:05:27 +0100

    Add s03-echo.cpp

---
+++ src/s03-echo.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+auto main(int argc, char *argv[]) -> int
+{
+    if (argc == 0)
+    {
+        return 1;
+    }
+
+    for (auto i = 1; i < argc; i++)
+    {
+        std::cout << " " << argv[i];
+    }
+
+    return 0;
+}
