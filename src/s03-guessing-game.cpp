9e5283c6fed3a06c3d8014a1192ea656be1ffa59
s22442 <73021231+s22442@users.noreply.github.com>
2020-11-07 13:17:54 +0100

    Update s03-guessing-game.cpp

--- src/s03-guessing-game.cpp
+++ src/s03-guessing-game.cpp
@@ -9,20 +9,26 @@ auto main() -> int
 
     auto const guess_me = d100(rd);
 
-    int input;
+    auto input = int{};
 
     while (true)
     {
-        std::cout << "\nguess: ";
+        std::cout << "guess: ";
         std::cin >> input;
 
         if (input == guess_me)
             break;
 
         if (input < guess_me)
+        {
             std::cout << "number too small!";
+        }
         else
+        {
             std::cout << "number too big!";
+        }
+
+        std::cout << "\n";
     }
 
     std::cout << "just right!";
