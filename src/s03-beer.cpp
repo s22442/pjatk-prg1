ec2f2acb9d5a86558d42373e1e3f31d4d2f31928
s22442 <73021231+s22442@users.noreply.github.com>
2020-11-07 12:19:40 +0100

    Add s03-beer.cpp

---
+++ src/s03-beer.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <cstdlib>
+
+auto main(int argc, char *argv[]) -> int
+{
+
+    int beer;
+
+    if (argv[1])
+        beer = atoi(argv[1]);
+    else
+        beer = 99;
+
+    for (auto i = beer; i > 0; i--)
+    {
+        std::cout << i << " bottles of beer on the wall, " << i << " bottles of beer.\n";
+        std::cout << "Take one down, pass it around, " << i - 1 << " bottles of beer on the wall...\n\n";
+    }
+
+    std::cout << "No more bottles of beer on the wall, no more bottles of beer.\n";
+    std::cout << "Go to the store and buy some more, " << beer << " bottles of beer on the wall...";
+
+    return 0;
+}
