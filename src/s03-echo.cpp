2135ed6f2d8effb17cca55d5435ba906dcc01746
BrOfRain <downloading130@gmail.com>
2020-11-08 17:10:07 +0100

    Update s03-echo.cpp

--- src/s03-echo.cpp
+++ src/s03-echo.cpp
@@ -1,14 +1,44 @@
 
+#include <algorithm>
 #include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
 
 auto main(int argc, char* argv[]) -> int
 {
-    if (argc == 0) {
-        return 1;
+    auto args = std::vector<std::string>{};
+    std::copy_n(argv, argc, std::back_inserter(args));
+
+    args.erase(args.begin());
+
+    auto print_newline = true;
+    auto reverse_args  = false;
+    auto separator     = " ";
+
+    if (args.at(0) == "-r") {
+        args.erase(args.begin());
+        reverse_args = true;
     }
 
-    for (auto i = 1; i < argc; i++) {
-        std::cout << " " << argv[i];
+    if (args.at(0) == "-l") {
+        args.erase(args.begin());
+        separator = "\n";
+    } else if (args.at(0) == "-n") {
+        args.erase(args.begin());
+        print_newline = false;
+    }
+
+    if (reverse_args) {
+        std::reverse(args.begin(), args.end());
+    }
+
+    for (auto const& each : args) {
+        std::cout << each << separator;
+    }
+
+    if (print_newline) {
+        std::cout << "\n";
     }
 
     return 0;
