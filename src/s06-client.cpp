47bd1870e8ec7b7b5951475631e0465738e4d17f
s22442 <s22442@pjwstk.edu.pl>
2021-01-12 19:08:47 +0100

    Update

--- src/s06-client.cpp
+++ src/s06-client.cpp
@@ -10,6 +10,37 @@
 #include <array>
 #include <iostream>
 #include <string>
+#include <thread>
+
+
+auto send_to_server(int const& sock) -> void
+{
+    std::cout << "Go ahead, type something:\n";
+
+    while (true) {
+        auto data = std::string{};
+        std::getline(std::cin, data);
+
+        if (data.empty()) {
+            break;
+        }
+
+        write(sock, data.data(), data.size());
+    }
+}
+
+
+auto read_from_server(int const& sock) -> void
+{
+    std::array<char, 512> buffer{};
+    while (auto const n = read(sock, buffer.data(), buffer.size())) {
+        auto const data = std::string{buffer.data(), buffer.data() + n};
+
+        std::cout << "DATA FROM THE SERVER:\n";
+        std::cout << data << "\n";
+    }
+}
+
 
 auto main() -> int
 {
@@ -24,7 +55,7 @@ auto main() -> int
     addr.sin_port   = htobe16(SERVER_PORT);
     inet_pton(addr.sin_family, SERVER_IP.c_str(), &addr.sin_addr);
 
-    std::cout << "Connectig to " << SERVER_IP << ":"
+    std::cout << "Connecting to " << SERVER_IP << ":"
               << std::to_string(SERVER_PORT) << " ...\n";
 
     auto server_sock =
@@ -37,31 +68,11 @@ auto main() -> int
         std::cout << "Send an empty line to terminate the connection and exit "
                      "the program\n";
 
-        while (true) {
-            {
-                auto data = std::string{};
+        auto to_server   = std::thread{send_to_server, std::ref(sock)};
+        auto from_server = std::thread{read_from_server, std::ref(sock)};
 
-                std::cout << "Send something to the server: ";
-                std::getline(std::cin, data);
-
-                if (data.empty()) {
-                    break;
-                }
-
-                write(sock, data.data(), data.size());
-            }
-            {
-                std::array<char, 512> buffer{};
-                auto const n = read(sock, buffer.data(), buffer.size());
-                if (n) {
-                    auto const data =
-                        std::string{buffer.data(), buffer.data() + n};
-
-                    std::cout << "DATA FROM THE SERVER:\n";
-                    std::cout << data << "\n";
-                }
-            }
-        }
+        to_server.join();
+        from_server.detach();
     }
 
     shutdown(sock, SHUT_RDWR);
